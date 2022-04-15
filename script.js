f1e3d89ceab2368b5807e99e95efec8f57c5cc97
Zain-ali0 <zain2alabdee2@gmail.com>
2022-04-15 23:40:15 +0300

    website

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('nav').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+        $('#menu').removeClass('fa-times');
+        $('nav').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 0 ){
+            $('header').addClass('sticky')
+        }else{
+            $('header').removeClass('sticky')
+        }
+    });
+
+    $('section').each(function(){
+        let top = $(window).scrollTop();
+        let offset = $(this).offset().top - 200;
+        let id = $(this).attr('id');
+        let height = $(this).height();
+        if(top > offset && top < offset + height){
+        $('.navbar a').removeClass('active');
+        $('.navbar').find(`[href="#${id}"]`).addClass('active');
+        }
+    });
+    
+    $('a[href*="#"]').on('click',function(e){
+        $('html, body').animate({
+        scrollTop : $($(this).attr('href')).offset().top,
+        },
+        500,
+        'linear'
+        );
+    
+    });
+});
